e11603234bab036a33be9a804c83b7ed95af8f0f
Vagner Oliveira <vagner.olliver@gmail.com>
2019-01-19 16:05:52 -0200

    name folder

--- src/app/root-store/root-store.module.ts
+++ src/app/root-store/root-store.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FeatureStoreModule } from './feature-store/feature-store.module';
+import { FeatureStoreModule } from './feature-store--entity/feature-store.module';
 
 @NgModule({
   declarations: [],
